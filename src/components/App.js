1ee0e5cae42f9fb227b1d0901421a1f402eadbc0
Kirsten Yoon <kirstenyoon@gmail.com>
2021-03-30 23:04:28 -0700

    added style loader dependency and linked scss stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../stylesheets/styles.scss";
 
 const App = () => {
-  return (
-    <div>Hello World!!</div>
-  )
-}
+	return <div>Hello World!!</div>;
+};
 
 export default App;
