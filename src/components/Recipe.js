89ebf7f0f2fe1cfcb78f9a5914c05a45ab516aa9
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-01 16:24:28 -0700

    fixed recipt buttons

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -79,148 +79,153 @@ class Recipe extends Component {
 
 		return (
 			<div className="recipes">
-				<div className="recipeHeader">
-					<div className="recipeHeaderText">
-						<p className="recipeTitles">{this.props.recipe.title}</p>
-						{/* Add link if exists */}
-						{this.props.recipe.link && (
-							<a
-								className="recipeLinks"
-								href={this.props.recipe.link}
-								target="_blank"
-							>
-								link
-							</a>
-						)}
+				<div>
+					<div className="recipeHeader">
+						<div className="recipeHeaderText">
+							<p className="recipeTitles">{this.props.recipe.title}</p>
+							{/* Add link if exists */}
+							{this.props.recipe.link && (
+								<a
+									className="recipeLinks"
+									href={this.props.recipe.link}
+									target="_blank"
+								>
+									link
+								</a>
+							)}
+						</div>
+						<button
+							onClick={() => {
+								this.props.deleteRecipe(this.props.recipe._id);
+							}}
+							className="deleteRecipeButton"
+						>
+							X
+						</button>
 					</div>
-					<button
-						onClick={() => {
-							this.props.deleteRecipe(this.props.recipe._id);
-						}}
-						className="deleteRecipeButton"
-					>
-						X
-					</button>
-				</div>
-				{this.props.recipe.ingredients && (
-					<p className="ingredientsLabel">Ingredients</p>
-				)}
-				<ul className="ingredientLists">{ingredientList}</ul>
-
-				{this.props.recipe.notes && (
-					<label className="notesLabel">Notes:</label>
-				)}
-				<p className="notes">{this.props.recipe.notes}</p>
-
-				<div className="ratingLine">
-					{this.props.recipe.rating && (
-						<label className="rating">Rating:</label>
+					{this.props.recipe.ingredients && (
+						<p className="ingredientsLabel">Ingredients</p>
 					)}
-					<p>{this.props.recipe.rating}</p>
+					<ul className="ingredientLists">{ingredientList}</ul>
+
+					{this.props.recipe.notes && (
+						<label className="notesLabel">Notes:</label>
+					)}
+					<p className="notes">{this.props.recipe.notes}</p>
 				</div>
 
-				<div className="recipeCardButtons">
-					<button
-						className="editButton"
-						onClick={() => {
-							this.toggleEditPopup();
-						}}
-					>
-						Edit
-					</button>
-					{this.state.showEditPopup && (
-						<EditRecipePopup
-							content={
-								<div className="editRecipePopup">
-									<b id="inputTitle">Edit Recipe</b>
-									<p></p>
-									<label className="inputLabels">Title: </label>
-									<input
-										id="title"
-										className="inputs"
-										type="text"
-										defaultValue={this.props.recipe.title}
-										placeholder="required"
-									></input>
-									<label className="inputLabels">Market: </label>
-									<input
-										id="category"
-										className="inputs"
-										type="text"
-										defaultValue={this.props.recipe.category}
-										placeholder="Any Market (default)"
-									></input>
-									<label className="inputLabels">Link: </label>
-									<input
-										id="link"
-										className="inputs"
-										type="text"
-										defaultValue={this.props.recipe.link}
-									></input>
-									<label className="inputLabels">Ingredients: </label>
-									<input
-										id="ingredients"
-										className="inputs"
-										type="text"
-										placeholder="separate ingredients by commas"
-										defaultValue={this.props.recipe.ingredients}
-									></input>
-									<label className="inputLabels">Notes: </label>
-									<input
-										id="notes"
-										className="inputs"
-										type="text"
-										defaultValue={this.props.recipe.notes}
-									></input>
-									<label className="inputLabels">Rating: </label>
-									<input
-										id="rating"
-										className="inputs"
-										type="text"
-										defaultValue={this.props.recipe.rating}
-									></input>
-									<p></p>
-									<button
-										onClick={() => {
-											const title = document.getElementById("title").value;
-											const category = document.getElementById("category")
-												.value;
-											const link = document.getElementById("link").value;
-											const ingredients = document.getElementById("ingredients")
-												.value;
-											const notes = document.getElementById("notes").value;
-											const rating = document.getElementById("rating").value;
-											this.editRecipe(
-												this.props.recipe._id,
-												title,
-												category,
-												link,
-												ingredients,
-												notes,
-												rating
-											);
-										}}
-									>
-										Update
-									</button>
-								</div>
-							}
-							handleClose={this.toggleEditPopup}
-						/>
-					)}
-					<button
-						className="addButton"
-						onClick={() => {
-							this.addToList(
-								this.props.recipe._id,
-								this.props.recipe.category,
-								this.props.recipe.ingredients
-							);
-							this.props.getRecipes();
-						}}
-					>
-						Add To List
-					</button>
+				<div className="recipeBottom">
+					<div className="ratingLine">
+						{this.props.recipe.rating && (
+							<label className="rating">Rating:</label>
+						)}
+						<p>{this.props.recipe.rating}</p>
+					</div>
+
+					<div className="recipeCardButtons">
+						<button
+							className="editButton"
+							onClick={() => {
+								this.toggleEditPopup();
+							}}
+						>
+							Edit
+						</button>
+						{this.state.showEditPopup && (
+							<EditRecipePopup
+								content={
+									<div className="editRecipePopup">
+										<b id="inputTitle">Edit Recipe</b>
+										<p></p>
+										<label className="inputLabels">Title: </label>
+										<input
+											id="title"
+											className="inputs"
+											type="text"
+											defaultValue={this.props.recipe.title}
+											placeholder="required"
+										></input>
+										<label className="inputLabels">Market: </label>
+										<input
+											id="category"
+											className="inputs"
+											type="text"
+											defaultValue={this.props.recipe.category}
+											placeholder="Any Market (default)"
+										></input>
+										<label className="inputLabels">Link: </label>
+										<input
+											id="link"
+											className="inputs"
+											type="text"
+											defaultValue={this.props.recipe.link}
+										></input>
+										<label className="inputLabels">Ingredients: </label>
+										<input
+											id="ingredients"
+											className="inputs"
+											type="text"
+											placeholder="separate ingredients by commas"
+											defaultValue={this.props.recipe.ingredients}
+										></input>
+										<label className="inputLabels">Notes: </label>
+										<input
+											id="notes"
+											className="inputs"
+											type="text"
+											defaultValue={this.props.recipe.notes}
+										></input>
+										<label className="inputLabels">Rating: </label>
+										<input
+											id="rating"
+											className="inputs"
+											type="text"
+											defaultValue={this.props.recipe.rating}
+										></input>
+										<p></p>
+										<button
+											onClick={() => {
+												const title = document.getElementById("title").value;
+												const category = document.getElementById("category")
+													.value;
+												const link = document.getElementById("link").value;
+												const ingredients = document.getElementById(
+													"ingredients"
+												).value;
+												const notes = document.getElementById("notes").value;
+												const rating = document.getElementById("rating").value;
+												this.editRecipe(
+													this.props.recipe._id,
+													title,
+													category,
+													link,
+													ingredients,
+													notes,
+													rating
+												);
+											}}
+										>
+											Update
+										</button>
+									</div>
+								}
+								handleClose={this.toggleEditPopup}
+							/>
+						)}
+						<button
+							className="addButton"
+							onClick={() => {
+								this.addToList(
+									this.props.recipe._id,
+									this.props.recipe.category,
+									this.props.recipe.ingredients
+								);
+								this.props.getRecipes();
+							}}
+						>
+							Add To List
+						</button>
+					</div>
 				</div>
 			</div>
 		);
