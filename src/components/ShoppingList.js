c74103d8a47ae74ce175a5598e61d6c68b60ae47
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-01 12:25:25 -0700

    shopping list is updated when add to list is clicked

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,18 +3,29 @@ import React, { Component } from "react";
 class ShoppingList extends Component {
 	render() {
 		console.log("SHOPPING LIST", this.props.shoppingList);
+
+		const items = [];
+		// Iterate over shopping list
+		for (let i = 0; i < this.props.shoppingList.length; i++) {
+			for (let j = 0; j < this.props.shoppingList[i].length; j++) {
+				// Push item to items array
+				items.push(
+					<Item key={"item" + i + j} item={this.props.shoppingList[i][j]} />
+				);
+			}
+		}
+
 		return (
 			<div id="shoppingListContainer">
 				<h2>Shopping List</h2>
-				<ul id="shoppingList">
-					<li>Item</li>
-					<li>Item</li>
-					<li>Item</li>
-					<li>Item</li>
-				</ul>
+				<ul id="shoppingList">{items}</ul>
 			</div>
 		);
 	}
+}
+
+function Item(props) {
+	return <li className="items">{props.item}</li>;
 }
 
 export default ShoppingList;
