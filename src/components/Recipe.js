f4dfe3d69d7d8302d7558b4d3ef96b9d2603faa9
Kirsten Yoon <kirstenyoon@gmail.com>
2021-03-31 21:28:43 -0700

    added ingredient list as bullet points

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -9,6 +9,17 @@ class Recipe extends Component {
 	}
 
 	render() {
+		console.log("HERE", this.props);
+		const ingredientList = [];
+		for (let i = 0; i < this.props.recipe.ingredients.length; i++) {
+			ingredientList.push(
+				<Ingredient
+					key={"ingredient" + i}
+					ingredient={this.props.recipe.ingredients[i]}
+				/>
+			);
+		}
+
 		return (
 			<div className="recipes">
 				<div className="recipeHeader">
@@ -35,7 +46,8 @@ class Recipe extends Component {
 					</button>
 				</div>
 				<p className="ingredientsLabel">Ingredients</p>
-				<p className="ingredientItems">{this.props.recipe.ingredients}</p>
+				{/* <p className="ingredientItems">{this.props.recipe.ingredients}</p> */}
+				<ul className="ingredientLists">{ingredientList}</ul>
 				<div className="ratingLine">
 					<label className="rating">Rating:</label>
 					{this.props.recipe.rating && <p>{this.props.recipe.rating}</p>}
@@ -60,6 +72,10 @@ class Recipe extends Component {
 			</div>
 		);
 	}
+}
+
+function Ingredient(props) {
+	return <li className="ingredients">{props.ingredient}</li>;
 }
 
 export default Recipe;
