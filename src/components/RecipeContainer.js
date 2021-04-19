2b281cefde808bcb88242d81b5551312b32135c0
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-19 11:10:04 -0700

    non-working version with hooks

--- src/components/RecipeContainer.js
+++ src/components/RecipeContainer.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
-import Recipe from "./Recipe";
+import React from 'react';
+import Recipe from './Recipe';
 
-class RecipeContainer extends Component {
-	constructor() {
-		super();
-	}
-
-	render() {
-		const recipes = [];
-		for (let i = 0; i < this.props.recipes.length; i++) {
-			recipes.push(
-				<Recipe
-					key={this.props.recipes[i].category + i}
-					recipeKey={this.props.recipes[i].category + i}
-					recipe={this.props.recipes[i]}
-					deleteRecipe={this.props.deleteRecipe}
-					getRecipes={this.props.getRecipes}
-					loadShoppingList={this.props.loadShoppingList}
-				/>
-			);
-		}
-
-		return (
-			<div className="marketDivs">
-				<h2>{this.props.market}</h2>
-				<div className="recipesContainer">{recipes}</div>
-			</div>
+const RecipeContainer = (props) => {
+	const recipes = [];
+	for (let i = 0; i < props.recipes.length; i++) {
+		recipes.push(
+			<Recipe
+				key={props.recipes[i].category + i}
+				recipeKey={props.recipes[i].category + i}
+				recipe={props.recipes[i]}
+				deleteRecipe={props.deleteRecipe}
+				getRecipes={props.getRecipes}
+				loadShoppingList={props.loadShoppingList}
+			/>
 		);
 	}
-}
+
+	return (
+		<div className="marketDivs">
+			<h2>{props.market}</h2>
+			<div className="recipesContainer">{recipes}</div>
+		</div>
+	);
+};
 
 export default RecipeContainer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import { HooksChromogenObserver } from 'chromogen';
 
-render(<App />, document.getElementById("root"));
+render(
+	<HooksChromogenObserver name="App">
+		<App />,
+	</HooksChromogenObserver>,
+	document.getElementById('root')
+);
