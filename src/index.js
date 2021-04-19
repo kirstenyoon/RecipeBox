d3ec1d467868392e4cb03b02fedcfb5b15855d58
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-19 11:56:24 -0700

    working version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import { HooksChromogenObserver } from 'chromogen';
 
-render(
-	<HooksChromogenObserver name="App">
-		<App />,
-	</HooksChromogenObserver>,
-	document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
